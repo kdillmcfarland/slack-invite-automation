65f5a3557424daf90201c5de0a518fd5753e259d
Bea Hernández <bea@Eriador.local>
2019-07-07 22:24:56 +0200

    fix responsive

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'R-Ladies Community',
-  // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'rladies-community.slack.com',
-  // access token of slack
-  // see https://github.com/outsideris/slack-invite-automation#issue-token
-  //
-  // You can test your token via curl:
-  //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
-  //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
-  //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN',
-  // an optional security measure - if it is set, then that token will be required to get invited.
-  inviteToken: process.env.INVITE_TOKEN || null,
-  // an optional security measure - if both are set, then recaptcha will be used.
-  recaptchaSiteKey: process.env.RECAPTCHA_SITE || null,
-  recaptchaSecretKey: process.env.RECAPTCHA_SECRET || null,
-  // default locale
-  locale: process.env.LOCALE || "en",
-  subpath: process.env.SUBPATH || "/"
+    // your community or team name to display on join page.
+    community: process.env.COMMUNITY_NAME || 'R-Ladies Community Slack',
+    // your slack team url (ex: socketio.slack.com)
+    slackUrl: process.env.SLACK_URL || 'rladies-community.slack.com',
+    // access token of slack
+    // see https://github.com/outsideris/slack-invite-automation#issue-token
+    //
+    // You can test your token via curl:
+    //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
+    //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
+    //   --compressed
+    slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN',
+    // an optional security measure - if it is set, then that token will be required to get invited.
+    inviteToken: process.env.INVITE_TOKEN || null,
+    // an optional security measure - if both are set, then recaptcha will be used.
+    recaptchaSiteKey: process.env.RECAPTCHA_SITE || null,
+    recaptchaSecretKey: process.env.RECAPTCHA_SECRET || null,
+    // default locale
+    locale: process.env.LOCALE || "en",
+    subpath: process.env.SUBPATH || "/"
 };
